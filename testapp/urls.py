6c46fd35bc8dd2df2b9d51421361ba079868c503
Babatunde Aremu <69739324+daveveed@users.noreply.github.com>
2023-11-25 11:23:04 +0100

    Delete testapp directory

--- testapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    # path('form/', views.DateFormview, name='form')
-    path('base/', views.base, name='base'),
-    path('base/<int:farm_id>', views.farm),
-]
